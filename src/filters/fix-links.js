9895682a1e65847e5737cb12c890ea079aa457ec
eito-fis <eamon@itofisher.com>
2021-12-15 01:38:58 -0500

    FIX: Broken MD links fixed

--- src/filters/fix-links.js
+++ src/filters/fix-links.js
@@ -2,16 +2,8 @@ const pathPrefix = process.env.PATH_PREFIX || '/';
 
 module.exports = value => {
   let newValue = value.replace(
-    /=\/images/g,
-    pathPrefix + 'images',
-  );
-  newValue = newValue.replace(
-    /=\/videos/g,
-    '=' + pathPrefix + 'videos',
-  );
-  newValue = newValue.replace(
-    /=\/what/g,
-    '=' + pathPrefix + 'what',
+    /="\/(images|videos|what)/g,
+    '="' + pathPrefix + '$1',
   );
   return newValue;
 };
